cd0e2b4516118fedb6d191825e2f84553ececb33
Natalia Gamez Barea <nataliagbarea@gmail.com>
2025-09-27 14:00:00 +0200

    Utilizacion de objeto en lugar de string

--- src/utilidades/Reservas.java
+++ src/utilidades/Reservas.java
@@ -11,8 +11,8 @@ public class Reservas {
     private String clase;
     // contador de todas las reservas que se han creado
     private static int contador_reservas = 0;
-    // lista donde se almacenan todas las reservas como Strings
-    private static ArrayList<String> reservas = new ArrayList<>();
+    // lista donde se almacenan todas las reservas como objetos
+    private static ArrayList<Reservas> reservas = new ArrayList<>();
 
     /**
      * Constructor de la clase Reservas
@@ -26,9 +26,8 @@ public class Reservas {
         // incrementamos el contador de reservas
         contador_reservas++;
 
-        // agregamos la reserva a la lista en formato String
-        // esto permite recorrer y mostrar todas las reservas sin tener que manejar objetos
-        reservas.add(this.toString());
+        // agregamos la reserva a la lista como objeto
+        reservas.add(this);
     }
 
     // convierte la reserva en un String separado por comas
@@ -40,42 +39,29 @@ public class Reservas {
 
     /**
      * Muestra todas las reservas en formato tabulado
-     * Separamos cada String en sus partes para poder alinear correctamente
      */
     public static void getMostrarReservas() {
         System.out.printf("%-20s%-20s%-20s%n", "NumeroAsiento", "NombrePasajero", "Clase");
 
-        for (String reserva : reservas) {
-            // separamos el String usando la coma como separador
-            // partes[0] = numeroAsiento, partes[1] = nombrePasajero, partes[2] = clase
-            String[] partes = reserva.split(",");
-
+        for (Reservas reserva : reservas) {
             // imprimimos cada columna con ancho fijo para que quede alineado
-            System.out.printf("%-20s%-20s%-20s%n", partes[0], partes[1], partes[2]);
+            System.out.printf("%-20s%-20s%-20s%n", reserva.numeroAsiento, reserva.nombrePasajero, reserva.clase);
         }
     }
 
     /**
      * Cuenta cuántas reservas hay de una clase específica
-     * Este método es especial porque necesitamos acceder a la columna "clase" directamente
-     * Por eso se hace split de cada String
      */
     public static int getCantidadReservasClase(String claseBuscada) {
         int contador_clase = 0;
 
-        for (String reserva : reservas) {
-            // separamos la reserva en columnas usando la coma como separador
-            // partes[0] = numeroAsiento, partes[1] = nombrePasajero, partes[2] = clase
-            String[] partes = reserva.split(",");
-
+        for (Reservas reserva : reservas) {
             // partes[2] corresponde a la columna "Clase"
-            // usamos equalsIgnoreCase para que no importe mayúsculas/minúsculas
-            if (partes[2].equalsIgnoreCase(claseBuscada)) {
+            if (reserva.clase.equalsIgnoreCase(claseBuscada)) {
                 contador_clase++;
             }
         }
 
-        // devolvemos la cantidad de reservas que coinciden con la clase buscada
         return contador_clase;
     }
 
